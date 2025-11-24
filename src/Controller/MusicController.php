87fac1460c7fd39ba1d5eb29d52660a9b6bf6985
Ing. Abel Bottello <abel@galaxyweb.dev>
2025-11-24 11:22:01 -0700

    Actualización 24 de noviembre de 2025

--- src/Controller/MusicController.php
+++ src/Controller/MusicController.php
@@ -9,11 +9,12 @@ use Cake\Http\Request;
 use Cake\Http\Response;
 
 
-class MusicController extends AppController {
-	
+class MusicController extends AppController
+{
+
 	private const string API_KEY = '2800fcd5956c46dfb55dd2686a96e6e7';
-	
-	private const array REQUEST_CONFIG = [
+
+	private const array EMBY_CONFIG = [
 		'scheme' => 'https',
 		'host' => 'emby.radiouas.org',
 		'port' => 8920,
@@ -22,87 +23,107 @@ class MusicController extends AppController {
 			'X-Emby-Token' => self::API_KEY,
 		],
 	];
-	
-	public function initialize() : void {
+
+	private int $maxDisplayedSongs = 15;
+
+	public function initialize(): void
+	{
 		parent::initialize();
 		$this->Authentication->allowUnauthenticated(['album', 'artist', 'playlist']);
 	}
-	
-	public function album() : Response {
+
+	public function album(): Response
+	{
 		$index = 1;
 		$albumID = $this->request->getQuery('ID');
 		$tracklist = boolval($this->request->getQuery('hide_tracklist'));
-		
-		$http = new Client(self::REQUEST_CONFIG);
+
+		$http = new Client(self::EMBY_CONFIG);
 		try {
-			$response = $http->get('/Items',
-									['ParentId' => $albumID, 'IncludeItemTypes' => 'Audio', 'Recursive' => 'true']);
+			$response = $http->get(
+				'/Items',
+				['ParentId' => $albumID, 'IncludeItemTypes' => 'Audio', 'Recursive' => 'true']
+			);
 
 			$playlist = json_decode($response->getStringBody());
-			
-			$cover = $http->buildURL('/Items/'.$albumID.'/Images/Primary', ['api_key' => self::API_KEY], $http->getConfig());
-			
+
+			$cover = $http->buildURL('/Items/' . $albumID . '/Images/Primary', ['api_key' => self::API_KEY], $http->getConfig());
+
 			$response = $http->get('/Items', ['Ids' => $albumID]);
 
 			$albumInfo = json_decode($response->getStringBody());
 
-			
+
 			$this->set(compact('http', 'playlist', 'albumInfo', 'cover', 'index'));
 			$this->set('api_key', self::API_KEY);
 			$this->set('bgColor', $this->request->getQuery('bgColor'));
 			$this->set('txtColor', $this->request->getQuery('txtColor'));
-			
+
 			$this->viewBuilder()->setLayout('widget');
-			$template = ($tracklist)? 'album_no_tracklist' : 'album';
-		}
-		catch(NetworkException $e){
+			$template = ($tracklist) ? 'album_no_tracklist' : 'album';
+		} catch (NetworkException $e) {
 			$template = 'widget_error';
-			$this->Flash->error('Error de conexión a la Fonoteca: '.$e->getMessage());
+			$this->Flash->error('Error de conexión a la Fonoteca: ' . $e->getMessage());
 		}
-		
+
 		return $this->render($template);
 	}
-	
-	public function artist() : Response {
+
+	public function artist(): Response
+	{
 		$index = 1;
-		$albumID = $this->request->getQuery('ID');
-		$http = new Client(self::REQUEST_CONFIG);
+		$artistID = $this->request->getQuery('ID');
+		$http = new Client(self::EMBY_CONFIG);
 
-		$response = $http->get('/Items',
-								['ParentId' => $albumID, 'IncludeItemTypes' => 'Audio', 'Recursive' => 'true']);
+		try {
+			$response = $http->get(
+				'/Items',
+				['ParentId' => $artistID, 'Limit' => $this->maxDisplayedSongs, 'SortBy' => 'Random', 'IncludeItemTypes' => 'Audio', 'Recursive' => 'true']
+			);
 
-		$playlist = json_decode($response->getStringBody());
-		
-		$cover = $http->buildURL('/Items/'.$albumID.'/Images/Primary',
-								['api_key' => self::API_KEY], $http->getConfig());
-		
-		$this->set(compact('http', 'playlist', 'cover', 'index'));
-		$this->set('api_key', self::API_KEY);
-		
+			$playlist = json_decode($response->getStringBody());
+
+			$cover = $http->buildURL(
+				'/Items/' . $artistID . '/Images/Primary',
+				['api_key' => self::API_KEY],
+				$http->getConfig()
+			);
+
+			$this->set(compact('http', 'playlist', 'cover', 'index'));
+			$this->set('api_key', self::API_KEY);
+			$template = 'artist';
+		} catch (NetworkException $e) {
+			$template = 'widget_error';
+			$this->Flash->error('Error de conexión a la Fonoteca: ' . $e->getMessage());
+		}
+
 		$this->viewBuilder()->setLayout('widget');
-		
-		return $this->render();
+
+		return $this->render($template);
 	}
-	
-	public function playlist() : Response {
+
+	public function playlist(): Response
+	{
 		$index = 1;
 		$playlistID = $this->request->getQuery('ID');
-		$http = new Client(self::REQUEST_CONFIG);
+		$http = new Client(self::EMBY_CONFIG);
 
-		$response = $http->get('/Playlists/'.$playlistID.'/Items',
-								['Limit' => 20]);
+		$response = $http->get(
+			'/Playlists/' . $playlistID . '/Items',
+			['Limit' => 20]
+		);
 
 		$playlist = json_decode($response->getStringBody());
-		
-		$cover = $http->get('/Items/'. $albumID .'/Images/Primary');
-		
+
+		$cover = $http->get('/Items/' . $albumID . '/Images/Primary');
+
 		$this->set(compact('http', 'playlist', 'cover', 'index'));
 		$this->set('api_key', self::API_KEY);
-		
+
 		$this->viewBuilder()->setLayout('widget');
-		
+
 		return $this->render();
 	}
-	
-	
+
+
 }
