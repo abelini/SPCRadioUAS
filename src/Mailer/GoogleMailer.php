4fcd586c0c24cb62c123cb6589e80318499c37ee
Ing. Abel Bottello <abel@galaxyweb.dev>
2025-11-26 10:33:31 -0700

    Implementada la clase GoogleTransport

--- src/Mailer/GoogleMailer.php
+++ src/Mailer/GoogleMailer.php
@@ -2,40 +2,34 @@
 
 namespace App\Mailer;
 
+use App\Mailer\Transport\GoogleTransport;
 use Cake\Mailer\Mailer;
 use Cake\Mailer\TransportFactory;
 
 
-class GoogleMailer extends Mailer {
-	
+class GoogleMailer extends Mailer
+{
+
 	private const array FROM = [
 		'radio@uas.edu.mx' => 'Dirección de Radio UAS'
 	];
-	
+
 	private const array CC = [
 		'abel@uas.edu.mx' => 'Ing. Abel Bottello',
 		'angelloperez@uas.edu.mx' => 'Angellos Pérez',
 		'brenda@uas.edu.mx' => 'Brenda Rodríguez',
 	];
-	
+
 	protected const array GENERAL_PROFILE = [
 		'from' => self::FROM,
 		'cc' => self::CC,
 		'emailFormat' => 'html',
 	];
-	
-	public function __construct() {
+
+	public function __construct()
+	{
 		parent::__construct(self::GENERAL_PROFILE);
-		
-		TransportFactory::setConfig('GoogleTransport', [
-			'host' => 'smtp.gmail.com',
-			'port' => 587,
-			'username' => 'radio@uas.edu.mx',
-			'password' => 'sfzvholhfjccuqaz',
-			'className' => 'Smtp',
-			'tls' => true,
-		]);
-		
-		$this->setTransport('GoogleTransport');
+
+		$this->setTransport(new GoogleTransport());
 	}
 }
--- src/Mailer/Transport/GoogleTransport.php
+++ src/Mailer/Transport/GoogleTransport.php
@@ -2,13 +2,14 @@
 
 namespace App\Mailer\Transport;
 
-use Cake\Mailer\AbstractTransport;
+use Cake\Mailer\Transport\SmtpTransport;
 use Cake\Mailer\Message;
 
 
-class GoogleTransport extends AbstractTransport {
-	
-	protected const array CONFIG = [
+class GoogleTransport extends SmtpTransport
+{
+
+	private const array CONFIG = [
 		'host' => 'smtp.gmail.com',
 		'port' => 587,
 		'username' => 'radio@uas.edu.mx',
@@ -16,14 +17,14 @@ class GoogleTransport extends AbstractTransport {
 		'className' => 'Smtp',
 		'tls' => true,
 	];
-	
-	
-	public function __construct() {
-		$this->setConfig(self::CONFIG);
+
+	public function __construct()
+	{
+		parent::__construct(self::CONFIG);
 	}
-	
-	public function send(Message $message): array {
-		//return parent::send($message); <-- send() es un método abstracto, no tiene implementación en la clase padre
-		return [];
+
+	public function send(Message $message): array
+	{
+		return parent::send($message);
 	}
 }
