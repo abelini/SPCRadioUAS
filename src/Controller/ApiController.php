b449cff1cd338b6e1f5985b77da922264a58a52b
Ing. Abel Bottello <abel@galaxyweb.dev>
2026-03-18 12:01:47 -0700

    Refactorizando codigo

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -12,6 +12,8 @@ class ApiController extends Controller
     public function initialize(): void
     {
         parent::initialize();
+
+        $this->components()->unload('FormProtection');
         $this->loadComponent('Flash');
     }
 
