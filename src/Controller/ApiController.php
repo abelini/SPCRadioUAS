ee5e7e956dc103e2063032ade6c8f940c2e6bc3b
Ing. Abel Bottello <abel@galaxyweb.dev>
2026-03-18 11:54:19 -0700

    Refactorizando codigo

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace SPC\Controller;
 
 use Cake\Controller\Controller;
+use Cake\Event\EventInterface;
 
 
 class ApiController extends Controller
@@ -11,6 +12,17 @@ class ApiController extends Controller
     public function initialize(): void
     {
         parent::initialize();
-        $this->loadComponent('Flash');
+        $this->loadComponent('RequestHandler');
+    }
+
+    /**
+     * Desactiva CSRF y seguridad para todos los endpoints API
+     */
+    public function beforeFilter(EventInterface $event)
+    {
+        parent::beforeFilter($event);
+
+        // Desactivar CSRF completamente para API
+        $this->getEventManager()->off($this->FormProtection);
     }
 }
